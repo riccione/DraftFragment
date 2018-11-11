b2d7be15cb23cb3ab353ad4a47fb07d04c9de6f5
Akio Ikeda <principewar@live.com>
2018-11-11 02:17:00 -0500

    Added /anim; fixed navigation to SettingsFragment; fixed some layouts; added methods to handle navigation in MainActivity

--- app/src/main/java/com/ignes/draftfragments/HomeFragment.kt
+++ app/src/main/java/com/ignes/draftfragments/HomeFragment.kt
@@ -2,30 +2,41 @@ package com.ignes.draftfragments
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass.
- *
+ * A main fragment
  */
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Navigation by action
+        /* Actions allow you to attach NavOptions in the navigation XML file, rather than
+        specifying them programmatically.
+        **Benefits:
+        You can visualize the navigation paths through your app
+        Actions can contain additional associated attributes you can set, such as a transition animation, arguments values, and backstack behavior
+        You can use the plugin safe args to navigate, which you'll see shortly
+        */
+        /*
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener {
+            //Navigation.createNavigateOnClickListener(R.id.next_action, null)
+            val action = HomeFragmentDirections.nextAction()
+            action.setFlowStepNumber(1)
+            findNavController().navigate(action)
+        }
+        */
+    }
 }
--- app/src/main/java/com/ignes/draftfragments/MainActivity.kt
+++ app/src/main/java/com/ignes/draftfragments/MainActivity.kt
@@ -1,85 +1,101 @@
 package com.ignes.draftfragments
 
+import android.content.res.Resources
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
+
+    private var drawerLayout: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        // Set up Action Bar
+        val navController = host.navController
+        setupActionBar(navController)
+
+        setupNavigationMenu(navController)
+
+        navController.addOnNavigatedListener { _, destination ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+            Log.d("NavigationActivity", "Navigated to $dest")
         }
-
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
     }
 
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+    private fun setupNavigationMenu(navController: NavController) {
+        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+        sideNavView?.setupWithNavController(navController)
+    }
+
+    private fun setupActionBar(navController: NavController) {
+        /*
+        This allows NavigationUI to decide what label to show in the action bar
+        And, since we are passing in drawerLayout, it will also determine whether to
+        show the up arrow or drawer menu icon
+        - Show a title in the ActionBar, based off of the destination's label
+        - Display the Up button when you are on any destination that's not the starting destination
+        - Display a drawer icon (hamburger icon) when you're on the start destination
+         */
+        drawerLayout = findViewById(R.id.drawer_layout)
+        setupActionBarWithNavController(navController, drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        /*
+        Allows NavigationUI to support proper up navigation or the drawer layout
+        drawer menu, depending on the situation
+         */
+        // TODO: check if it works
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(drawerLayout)
+        //return drawerLayout.navigateUp(findNavController(R.id.nav_host_fragment))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+        val retValue = super.onCreateOptionsMenu(menu)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        // The NavigationView already has these same navigation items, so we only add
+        // navigation items to the menu here if there isn't a NavigationView
+        if (navigationView == null) {
+            menuInflater.inflate(R.menu.activity_main_drawer, menu)
+            return true
+        }
+        return retValue
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
+        /*
+        Navigation UI Handle the item selection:
+        Have the NavigationUI look for an action or destination matching the menu
+        item id and navigate there if found.
+        Otherwise, bubble up to the parent.
+         */
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 }
