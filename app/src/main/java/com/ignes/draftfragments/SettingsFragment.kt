c687046753d737c7ac3c778f1073d285bba8bd5b
Akio Ikeda <principewar@live.com>
2018-12-07 02:04:16 -0500

    Added Settings Preferences

--- app/src/main/java/com/ignes/draftfragments/SettingsFragment.kt
+++ app/src/main/java/com/ignes/draftfragments/SettingsFragment.kt
@@ -1,15 +1,26 @@
 package com.ignes.draftfragments
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
 /**
  * Settings fragment
+ * TODO: test this part
  */
 class SettingsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() : SettingsFragment {
+            return SettingsFragment()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,4 +31,35 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+    class SettingsPreferenceFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            // Load the preferences from an XML resource
+            setPreferencesFromResource(R.xml.preferences, rootKey)
+
+            val numberOfQuestions = findPreference(getString(R.string.settings_number_of_questions_by_key))
+            bindPreferenceSummaryToValue(numberOfQuestions)
+        }
+
+        override fun onPreferenceChange(preference: Preference, value: Any): Boolean {
+            val stringValue = value.toString()
+            if (preference is ListPreference) {
+                val prefIndex = preference.findIndexOfValue(stringValue)
+                if (prefIndex >= 0) {
+                    val labels = preference.entries
+                    preference.setSummary(labels[prefIndex])
+                }
+            } else {
+                preference.summary = stringValue
+            }
+            return true
+        }
+
+        private fun bindPreferenceSummaryToValue(preference: Preference) {
+            preference.onPreferenceChangeListener = this
+            val preferences = PreferenceManager.getDefaultSharedPreferences(preference.context)
+            val preferenceString = preferences.getString(preference.key, "")
+            onPreferenceChange(preference, preferenceString)
+        }
+    }
 }
